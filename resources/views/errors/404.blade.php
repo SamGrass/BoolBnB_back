53afe845d0142d295693c6d82f70e98dee6b59d5
Samuele <samueletasciotti@icloud.com>
2024-10-24 11:31:37 +0200

    fix: error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,6 +7,6 @@
     <div class="error-page">
         <h1>404</h1>
         <p>Spiacente, la pagina che stai cercando non esiste.</p>
-        <a href="{{ url('/') }}" class="btn btn-primary">Torna alla home</a>
+        <a href="{{ url('/admin') }}" class="btn btn-primary">Torna nella dashboard</a>
     </div>
 @endsection
