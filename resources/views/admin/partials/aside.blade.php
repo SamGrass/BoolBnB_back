38dcc23fd5a3072f547ea5bad6c6b7d7a78bfe87
Samuele <samueletasciotti@icloud.com>
2024-10-24 18:36:09 +0200

    fix: aside icons

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,9 +1,9 @@
 <aside class="text-bg-dark p-3">
     <ul class="aside-nav d-flex flex-column gap-2">
         <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a></li>
-        <li><a href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-house"></i> Appartamenti</a></li>
-        <li><a href="{{ route('admin.apartments.create') }}"><i class="fa-solid fa-house"></i> Inserisci appartamento</a></li>
-        <li><a href="{{ route('admin.messages.index') }}"><i class="fa-solid fa-house"></i> Messaggi</a></li>
+        <li><a href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-building-user"></i> Appartamenti</a></li>
+        <li><a href="{{ route('admin.apartments.create') }}"><i class="fa-solid fa-plus"></i> Inserisci appartamento</a></li>
+        <li><a href="{{ route('admin.messages.index') }}"><i class="fa-solid fa-message"></i> Messaggi</a></li>
        
     </ul>
 </aside>
